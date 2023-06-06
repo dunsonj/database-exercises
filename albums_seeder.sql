5eeaadc32bcd31c3c41a6a013b9d687279055ce1
Jakira Dunson <jakiradunson@gmail.com>
2023-06-06 14:51:17 -0500

    select exercise

---
+++ albums_seeder.sql
@@ -0,0 +1,11 @@
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES  ('Whitney Houston', 'Whitney', '1987', '25000000', 'Pop & R&B');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Bad', '1987', '35000000', 'Pop'),
+ ('Adele', '21', '2011', '31000000', 'Pop'),
+ ('Michael Jackson', 'Thriller', '1982', '70000000', 'Pop'),
+ ('Eminem', 'The Eminem Show', '2002', '27000000', 'Hip Hop');
+
+SELECT * FROM albums;
+
