992eabe8849ae8b92006df32f26b80fdaf4ab885
Jakira Dunson <jakiradunson@gmail.com>
2023-06-07 14:38:08 -0500

    where in mysql

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 SELECT first_name FROM employees ORDER BY  first_name ASC;
 
 SELECT first_name,last_name FROM employees ORDER BY first_name ASC, last_name DESC;
--- where_exercises.sql
+++ where_exercises.sql
@@ -2,4 +2,10 @@ SELECT first_name FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
 Select * FROM employees WHERE last_name = 'E%';
 
-Select * FROM employees WHERE last_name = '%d%';
+Select * FROM employees WHERE last_name = '%q%';
+
+SELECT first_name FROM employees WHERE first_name = ('Irena')  OR first_name = ('Vidya')  OR first_name =('Maya');
+
+Select * FROM employees WHERE last_name = '%E'OR last_name = 'E%';
+
+Select * FROM employees WHERE last_name NOT LIKE '%qu%';
