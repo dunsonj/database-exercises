bc0b25e864c0d00813b6a69b0b350452e3a1a690
Jakira Dunson <jakiradunson@gmail.com>
2023-06-07 13:52:49 -0500

    where in mysqul clauses

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,4 +1,4 @@
-SELECT first_name FROM employees where first_name IN ('Irena', 'Vidya', 'Maya');
+SELECT first_name FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
 Select * FROM employees WHERE last_name = 'E%';
 
