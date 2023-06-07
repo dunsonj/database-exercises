bc0e2fe9048c249e86e9db092bcb31fe2b7b3b80
Jakira Dunson <jakiradunson@gmail.com>
2023-06-07 16:22:04 -0500

    where in mysql edits

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,9 +1,7 @@
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name, last_name;
 
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name, first_name;
 
+SELECT first_name FROM employees WHERE  last_name LIKE '%e%' ORDER BY emp_no;
 
-
-
-
-SELECT first_name FROM employees ORDER BY  first_name ASC;
-
-SELECT first_name,last_name FROM employees ORDER BY first_name ASC, last_name DESC;
+SELECT first_name FROM employees WHERE  last_name LIKE '%e%' ORDER BY emp_no DESC;
--- where_exercises.sql
+++ where_exercises.sql
@@ -1,11 +1,11 @@
-SELECT first_name FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
-Select * FROM employees WHERE last_name = 'E%';
+Select * FROM employees WHERE last_name LIKE 'E%';
 
-Select * FROM employees WHERE last_name = '%q%';
+Select * FROM employees WHERE last_name LIKE '%q%';
 
 SELECT first_name FROM employees WHERE first_name = ('Irena')  OR first_name = ('Vidya')  OR first_name =('Maya');
 
-Select * FROM employees WHERE last_name = '%E'OR last_name = 'E%';
+Select * FROM employees WHERE last_name = 'E%' OR last_name = '%E';
 
-Select * FROM employees WHERE last_name NOT LIKE '%qu%';
+Select * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
